136372830e3830199f9274e6a8f85e747ad2ea7e
AGTHARN <supscrusjus@gmail.com>
2022-08-07 08:42:42 +0800

    style: improve design of header and license notice

--- plugins/RotateWrench/src/SolicitDev/RotateWrench/EventListener.php
+++ plugins/RotateWrench/src/SolicitDev/RotateWrench/EventListener.php
@@ -1,27 +1,27 @@
 <?php
 
-/**
- *          █▀ █▀█ █░░ █ █▀▀ █ ▀█▀
- *          ▄█ █▄█ █▄▄ █ █▄▄ █ ░█░
- * 
- * █▀▄ █▀▀ █░█ █▀▀ █░░ █▀█ █▀█ █▀▄▀█ █▀▀ █▄░█ ▀█▀
- * █▄▀ ██▄ ▀▄▀ ██▄ █▄▄ █▄█ █▀▀ █░▀░█ ██▄ █░▀█ ░█░
- *    https://github.com/Solicit-Development
- * 
- *    Copyright 2022 Solicit-Development
- *    Licensed under the Apache License, Version 2.0 (the 'License');
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- * 
- *        http://www.apache.org/licenses/LICENSE-2.0
- * 
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an 'AS IS' BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- * 
- */
+/**                                                                        **\
+ *                        █▀ █▀█ █░░ █ █▀▀ █ ▀█▀                            *
+ *                        ▄█ █▄█ █▄▄ █ █▄▄ █ ░█░                            *
+ *                                                                          *
+ *            █▀▄ █▀▀ █░█ █▀▀ █░░ █▀█ █▀█ █▀▄▀█ █▀▀ █▄░█ ▀█▀                *
+ *            █▄▀ ██▄ ▀▄▀ ██▄ █▄▄ █▄█ █▀▀ █░▀░█ ██▄ █░▀█ ░█░                *
+ *                https://github.com/Solicit-Development                    *
+ *                                                                          *
+ *                  Copyright 2022 Solicit-Development                      *
+ *    Licensed under the Apache License, Version 2.0 (the 'License');       *
+ *   you may not use this file except in compliance with the License.       *
+ *                                                                          *
+ *                You may obtain a copy of the License at                   *
+ *              http://www.apache.org/licenses/LICENSE-2.0                  *
+ *                                                                          *
+ *  Unless required by applicable law or agreed to in writing, software     *
+ *   distributed under the License is distributed on an 'AS IS' BASIS,      *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ *  See the License for the specific language governing permissions and     *
+ *                    limitations under the License.                        *
+ *                                                                          *
+ */                                                                        //
 
 declare(strict_types=1);
 
@@ -34,8 +34,8 @@ use pocketmine\event\player\PlayerInteractEvent;
 class EventListener implements Listener
 {
     /**
-     * @priority MONITOR
-     */
+    * @priority MONITOR
+    */
     public function onPlayerInteract(PlayerInteractEvent $event): void
     {
         $player = $event->getPlayer();
@@ -46,8 +46,8 @@ class EventListener implements Listener
     }
 
     /**
-     * @priority HIGHEST
-     */
+    * @priority HIGHEST
+    */
     public function onBlockBreak(BlockBreakEvent $event): void
     {
         if ($event->getItem()->getNamedTag()->getString('RotateWrench', '') === 'Wrench') {
